0c265de33e4b16b0669382c79653b5eca395e25b
Techno Crypto <techno.cryptospy@gmail.com>
2023-06-03 16:31:20 +0530

    server which serves  hsl player to play m3u8 video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
-import VideoPlayer from './VideoPlayer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Player } from 'video-react';
+import 'video-react/dist/video-react.css';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+import 'videojs-contrib-hls';
 
 const App = () => {
   useEffect(() => {
@@ -8,23 +13,34 @@ const App = () => {
     console.log('A user connected');
   }, []);
 
-  const video = {
-    src: 'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
-    title: 'Sample Video'
-  };
-
   return (
     <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-8">
-          <h1 className="mt-4 mb-4 text-center">Video Player</h1>
-          <div className="card">
-            <div className="card-body">
-              <VideoPlayer src={video.src} title={video.title} />
-            </div>
+      <header className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            Video Player
+          </a>
+        </div>
+      </header>
+
+      <main>
+        <div className="jumbotron">
+          <h1 className="display-4">Welcome to Video Player</h1>
+          <p className="lead">Experience seamless video playback with our player.</p>
+        </div>
+
+        <div className="card mx-auto" style={{ maxWidth: '400px' }}>
+          <div className="card-body">
+            <Player src="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8" />
           </div>
         </div>
-      </div>
+      </main>
+
+      <footer className="bg-dark text-white text-center py-3 mt-5">
+        <div className="container">
+          <p>© 2023 Your Company. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 };
---
+++ src/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      // Add more rules for other file types if needed (e.g., CSS, images)
+    ],
+  },
+};
